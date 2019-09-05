dfe1522f34f92665ce65c782605e22fef796d75e
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-09-05 16:11:15 -0400

    get click event rendering content from props for each menu, hiding otherwise

--- src/state-drills/Accordian.jsx
+++ src/state-drills/Accordian.jsx
@@ -6,10 +6,30 @@ export default class Accordian extends React.Component {
     sections: []
   }
 
+  state = {
+    currentMenuIndex: null
+  }
+
+  handleButtonClick = (index) => {
+    console.log('you clicked button', index)
+    this.setState({
+      currentMenuIndex: index
+    })
+  }
+
+  renderMenuIndexContent = () => {
+    return (
+      <p>
+        {this.state.currentMenuIndex !== null && this.props.sections[this.state.currentMenuIndex].content}
+      </p>
+    )
+  }
+
   renderMenuItems() {
     const menu = this.props.sections.map((item, index) => (
       <li key={index}>
-        <button>{item.title}</button>
+        <button key={index} onClick={() => this.handleButtonClick(index)}>{item.title}</button>
+        {this.state.currentMenuIndex === index && this.renderMenuIndexContent()}
       </li>
     ))
     return menu
