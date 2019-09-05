a593296fb1ba340b6c1d5dbd4da009a00e9c6b98
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-09-05 14:56:41 -0400

    get Tabs snapshot for default working

--- src/accordian-drill/Tabs.jsx
+++ src/accordian-drill/Tabs.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from 'react'
 
 export default class Tabs extends React.Component {
-  static defaultProps = { tabs: [] };
- render() {
-   const buttons = this.props.tabs.map((tab, index) => (
-     <button key={index}>
-       {tab.name}
-     </button>
-   ))
-   return <div>
-     {buttons}
-   </div>
- }
+  static defaultProps = { tabs: [] }
+  render() {
+    const buttons = this.props.tabs.map((tab, index) => (
+      <button key={index}>{tab.name}</button>
+    ))
+    const currentTab = this.props.tabs[0]
+    return (
+      <div>
+        {buttons}
+          {this.props.tabs.length && (
+            <div>
+              {currentTab.content}
+            </div>
+          )}
+      </div>
+    )
+  }
 }
