054592f23f671167ca3724a6eb231e7c14859a37
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-09-05 15:59:14 -0400

    get li rendering on page using props

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import React from 'react';
 import Accordian from './state-drills/Accordian';
 
 function App() {
+  const sections = [
+    {
+      title: 'Section 1',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    },
+    {
+      title: 'Section 2',
+      content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+    },
+    {
+      title: 'Section 3',
+      content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+    },
+  ];
+  
   return (
     <main className='App'>
-      <Accordian />
+      <Accordian sections={sections} />
     </main>
   );
 }
--- src/state-drills/Accordian.jsx
+++ src/state-drills/Accordian.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
 export default class Accordian extends React.Component {
+
+  static defaultProps = {
+    sections: []
+  }
+
+  renderMenuItems() {
+    const menu = this.props.sections.map((item, index) => (
+      <li key={index}>
+        <button>{item.title}</button>
+      </li>
+    ))
+    return menu
+  }
+
   render() {
     return (
-      <div></div>
+      <ul>
+        {this.renderMenuItems()}
+      </ul>
     )
   }
 }
