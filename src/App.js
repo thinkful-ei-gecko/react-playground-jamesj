d6af2100c2153b07c4d1c212e380099ac1150d6c
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-09-05 15:44:40 -0400

    add Accordian component with render test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import Tabs from './event-handler-practice/Tabs';
-
-const tabsProp = [
-  { name: 'First tab',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
-  { name: 'Second tab',
-    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
-  { name: 'Third tab',
-    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
-];
+import Accordian from './state-drills/Accordian';
 
 function App() {
   return (
     <main className='App'>
-      <Tabs tabs={tabsProp}/>
+      <Accordian />
     </main>
   );
 }
---
+++ src/state-drills/Accordian.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Accordian extends React.Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
